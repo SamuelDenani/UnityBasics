743ab37c68e5e599c2644748f891cbfb93dfd6fb
Samuel Denani <samueldenani2@hotmail.com>
2020-12-29 13:20:48 -0300

    Adding gitignore and readme files to project files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] Transform GroundCheckTransform;
+    [SerializeField] LayerMask playerMask;
+
+    Rigidbody rb;
+
+    int superJumpsRemaining;
+    bool jumpKeyWasPressed;
+    float horizontalInput;
+    bool isGrounded;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        isGrounded = Physics.CheckSphere(GroundCheckTransform.position, 0.1f, playerMask);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jumpKeyWasPressed = Input.GetKeyDown(KeyCode.Space);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate() {
+        
+        if (jumpKeyWasPressed && isGrounded) {
+            float jumpForce = 270f;
+
+            if (superJumpsRemaining > 0) {
+                jumpForce *= 3;
+                superJumpsRemaining--;
+            }
+            rb.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        rb.velocity = new Vector3(horizontalInput, rb.velocity.y, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == 9) {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Unity Basics
+
+## en-US
+
+This project was created following [this video](https://www.youtube.com/watch?v=pwZpJzpE2lQ) from [Imphenzia's channel](https://www.youtube.com/user/ImphenziaMusic).
+
+In this project I learned the most basic concepts about Unity, such as creating game objects, lighting, prefabs, materials, C# scripts, physics, physics materials etc.
+
+I intend to use this repo to try and make new things in Unity as I learn something new or try to improve some skill I already have so I'll try to keep it updated.
+
+---
+
+## pt-BR
+
+Esse projeto foi criado seguindo [esse vídeo](https://www.youtube.com/watch?v=pwZpJzpE2lQ) do [canal Imphenzia](https://www.youtube.com/user/ImphenziaMusic).
+
+Nesse projeto eu aprendi conceitos básicos da Unity, como criar game objects, iluminação, prefabs, materiais, scripts em C#, física, materiais de física etc.
+
+Eu pretendo usar esse repostório para tentar coisas novas na Unity conforme eu for aprendendo ou estiver tentando melhorar algo que eu já sei fazer, então vou tentar manter o repositório sempre atualizado.
